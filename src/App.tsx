60c06b9da5dd5b6b7f6a6a06bbc9eb4db36ffb50
Rayron Rodffer <r.rodffer@gmail.com>
2020-06-17 15:59:55 -0300

    github-explorer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 
 const App: React.FC = () => (
   <>
-    <BrowserRouter>
+    <BrowserRouter basename="github-explorer">
       <Routes />
     </BrowserRouter>
     <GlobalStyle />
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 import Repository from '../pages/Repository';
 
 const Routes: React.FC = () => (
-  <BrowserRouter basename="github-explorer">
-    <Switch>
-      <Route path="/" exact component={Dashboard} />
-      <Route path="/repositories/:repository+" component={Repository} />
-    </Switch>
-  </BrowserRouter>
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repositories/:repository+" component={Repository} />
+  </Switch>
 );
 
 export default Routes;
